ef3f1d53232f8daf54d92c58094c651ddad91868
Zeik Tan Yong Xiang <zeikxiang@gmail.com>
2014-12-27 13:09:04 +0800

    # history api updated for <title> change

--- mingstar/js/main.js
+++ mingstar/js/main.js
@@ -7,22 +7,76 @@
         - http://rosspenman.com/pushstate-jquery/
 */
 $(document).ready(function() {
-    init = function() {
-        // Do this when a page loads
-    },
-
-    loadPage = function(href) {
-        if (href=='index.html')
-            href = 'home.html'
-
-        $('#content').load(href, function(responseTxt, statusTxt, xhr) {
-            if (statusTxt=='success') {
-                console.log('load complete');
-            }
-            if (statusTxt=='error')
-                console.log('Error: '+xhr.status+": "+xhr.statusTxt);
+    jQuery.cachedScript = function(url, options) {
+         // Allow user to set any option except for dataType, cache, and url
+        options = $.extend( options || {}, {
+            dataType: 'script',
+            cache: true,
+            url: url
         });
+        // Use $.ajax() since it is more flexible than $.getScript
+        // Return the jqXHR object so we can chain callbacks
+        return jQuery.ajax( options );
     };
+
+    String.prototype.decodeHTML = function() {
+        return $('div', {html: ''+this}).html();
+    }
+
+    var $content = $('#content'),
+
+        init = function() {
+            // Do this when a page loads
+            console.log('init call');
+
+            $content.load('home.html #main>*', function(responseTxt, statusTxt, xhr) {
+                if (statusTxt=='success')
+                    console.log('initialization complete');
+                if (statusTxt=='error')
+                    console.lop('Error: Fail to load home.html.');
+            });
+        },
+
+        ajaxLoad = function(html) {
+            console.log('ajaxLoad called');
+            document.title = html
+                .match(/<title>(.*?)<\/title>/)[1]
+                .trim();
+
+            // init();
+        },
+
+        loadPage = function(href) {
+            if (href=='index.html')
+                href = 'home'
+
+            $content.load(href+'.html #main>*', function(responseTxt, statusTxt, xhr) {
+                if (statusTxt=='success') {
+                    console.log('load complete');
+                    ajaxLoad(responseTxt);
+                }
+                if (statusTxt=='error')
+                    console.log('Error: '+xhr.status+": "+xhr.statusTxt);
+            });
+
+            // $.load('title.html #'+href, function(responseTxt, statusTxt, xhr) {
+            //     if (statusTxt=='success')
+            //         ajaxLoad(responseTxt);
+            //     if (statusTxt=='error')
+            //         console.log('Error: '+xhr.status+": "+xhr.statusTxt);
+            // });
+
+            // $.cachedScript('js/'+href+'.js').done(function(script, textStatus) {
+            //     console.log(textStatus);
+            // });
+
+            $.getScript('js/'+href+'.js', function(data, textStatus, jqxhr) {
+                // console.log(data);
+                // console.log(textStatus);
+                // console.log(jqxhr);
+                console.log('script loaded');
+            });
+        };
 
     init();
 
@@ -42,11 +96,11 @@ $(document).ready(function() {
     });
 
     $(window).on('popstate', function(evt) {
-        loadPage(location.href.split('/').pop())
+        // if (evt.originalEvent.state !== null)
+            loadPage(location.href.split('/').pop());
     });
 
     $('#navbar-collapse-main').on('click', 'a', function(evt) {
-        // evt.preventDefault();
         // console.log('item ID: '+$(this).attr('id'));
         var href = $(this).attr('href');
 
