8207e890ffc55e396faad0a152683203f4191886
Zeik Tan Yong Xiang <zeikxiang@gmail.com>
2015-01-05 12:05:00 +0800

    # Testing for bootstrap-lightbox-v0.7.0 in gallery # still need enhancement to should different selected images in lightbox

---
+++ mingstar/js/gallery.js
@@ -0,0 +1,8 @@
+
+// $('#demoLightbox').lightbox();
+// prettyPrint();
+
+// $(document).delegate('*[data-toggle="lightbox"]', 'click', function(evt) {
+//     evt.preventDefault();
+//     $(this).lightbox();
+// }); 
