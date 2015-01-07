b586324e2bb6d281dbfbc439daf5fef55757e66d
Zeik Tan Yong Xiang <zeikxiang@gmail.com>
2015-01-08 00:54:56 +0800

    # Update about.html content

--- mingstar/js/aboutus.js
+++ mingstar/js/aboutus.js
@@ -8,8 +8,8 @@ $('#readmore').click(function(evt) {
 
 imageSlider.reload();
 
-$.getScript('http://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=461&amp;locationId=1222443&amp;lang=en_MY&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2', function() {
-    if (typeof(window.taValidate) != 'undefined') {
-      window.taValidate();
-    }
-});
+// $.getScript('http://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=461&amp;locationId=1222443&amp;lang=en_MY&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2', function() {
+//     if (typeof(window.taValidate) != 'undefined') {
+//       window.taValidate();
+//     }
+// });
