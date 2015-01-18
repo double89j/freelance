c68761dab5db6aab43c9a0c8f12980ab89cdf4ef
Zeik Tan Yong Xiang <zeikxiang@gmail.com>
2015-01-18 21:39:37 +0800

    # Gallery thumbnail slider model enhanced

--- mingstar/js/gallery.js
+++ mingstar/js/gallery.js
@@ -10,15 +10,15 @@
 // jQuery(document).ready(function ($) {
 
     var _SlideshowTransitions = [
-    //Fade
-    { $Duration: 1200, $Opacity: 2 }
+        //Fade
+        { $Duration: 1200, $Opacity: 2 }
     ];
 
     var options = {
         $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
         $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
         $AutoPlayInterval: 3000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-        $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
+        $PauseOnHover: 1,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
 
         $ArrowKeyNavigation: true,                          //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
         $SlideDuration: 500,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
@@ -36,40 +36,66 @@
             $Class: $JssorSlideshowRunner$,                 //[Required] Class to create instance of slideshow
             $Transitions: _SlideshowTransitions,            //[Required] An array of slideshow transitions to play slideshow
             $TransitionsOrder: 1,                           //[Optional] The way to choose transition to play slide, 1 Sequence, 0 Random
-            $ShowLink: true                                    //[Optional] Whether to bring slide link on top of the slider when slideshow is running, default value is false
+            $ShowLink: true                                 //[Optional] Whether to bring slide link on top of the slider when slideshow is running, default value is false
         },
 		
 		$ThumbnailNavigatorOptions: {
-                $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
-                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $Class: $JssorThumbnailNavigator$,          //[Required] Class to create thumbnail navigator instance
+                    $ChanceToShow: 2,                       //[Required] 0 Never, 1 Mouse Over, 2 Always
 
-                    $Loop: 2,                                       //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
-                    $SpacingX: 3,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
-                    $SpacingY: 3,                                   //[Optional] Vertical space between each thumbnail in pixel, default value is 0
-                    $DisplayPieces: 8,                              //[Optional] Number of pieces to display, default value is 1
+                    $Loop: 2,                               //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
+                    $SpacingX: 3,                           //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
+                    $SpacingY: 3,                           //[Optional] Vertical space between each thumbnail in pixel, default value is 0
+                    $DisplayPieces: 8,                      //[Optional] Number of pieces to display, default value is 1
                     $ParkingPosition: 233
             },
 		
-        $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
-            $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+        $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+            $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
             $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
             $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
             $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
             $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-            $SpacingX: 10,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-            $SpacingY: 10,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+            $SpacingX: 10,                                  //[Optional] Horizontal space between each item in pixel, default value is 0
+            $SpacingY: 10,                                  //[Optional] Vertical space between each item in pixel, default value is 0
             $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
         },
 
         $ArrowNavigatorOptions: {
-            $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+            $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
             $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
             $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
             $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
         }
     };
+    var superior_options = {
+        $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+        $AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+        $SlideDuration: 500,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+        $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+        $UISearchMode: 0,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
+            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+
+            $Loop: 2,                                       //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
+            $SpacingX: 6,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
+            $SpacingY: 3,                                   //[Optional] Vertical space between each thumbnail in pixel, default value is 0
+            $DisplayPieces: 7,                              //[Optional] Number of pieces to display, default value is 1
+            $ParkingPosition: 204,                          //[Optional] The offset position to park thumbnail,
+
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+                $ChanceToShow: 2,                           //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 2,                             //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 6                                   //[Optional] Steps to go for each navigation request, default value is 1
+            }
+        }
+    };
+
     var standard_slider = new $JssorSlider$("standard_container", options);
-	var superior_slider = new $JssorSlider$("superior_container", options);
+    var superior_slider = new $JssorSlider$("superior_container", superior_options);
     var deluxe_slider = new $JssorSlider$("deluxe_container", options);
 
     var options2 = {
@@ -116,23 +142,33 @@
     //responsive code begin
     //you can remove responsive code if you don't want the slider scales while window resizes
     function ScaleSlider() {
+        // Standard
         var standardParentWidth = standard_slider.$Elmt.parentNode.clientWidth;
         if (standardParentWidth)
             standard_slider.$ScaleWidth(Math.min(standardParentWidth, 960));
         else
             window.setTimeout(ScaleSlider, 30);
 
+        // Superior
         var superiorParentWidth = superior_slider.$Elmt.parentNode.clientWidth;
         if (superiorParentWidth)
             superior_slider.$ScaleWidth(Math.min(superiorParentWidth, 960));
         else
             window.setTimeout(ScaleSlider, 30);
 
+        // Deluxe
         var deluxeParentWidth = deluxe_slider.$Elmt.parentNode.clientWidth;
         if (deluxeParentWidth)
             deluxe_slider.$ScaleWidth(Math.min(deluxeParentWidth, 960));
         else
             window.setTimeout(ScaleSlider, 30);
+
+        // My Deluxe
+        // var my_deluxe_container = my_deluxe_slider.$Elmt.parentNode.clientWidth;
+        // if (my_deluxe_container)
+        //     my_deluxe_slider.$ScaleWidth(Math.min(my_deluxe_container, 960));
+        // else
+        //     window.setTimeout(ScaleSlider, 30);
 
         // var bodyWidth = document.body.clientWidth;
         // if (bodyWidth)
