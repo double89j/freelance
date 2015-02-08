4b3a24d6432033ee407f0afe60a8ea15163f7721
Zeik Tan Yong Xiang <zeikxiang@gmail.com>
2015-02-08 15:40:04 +0800

    # TripAdvisor added # Places added with waze app open link button

--- mingstar/js/places.js
+++ mingstar/js/places.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+    function MM_openBrWindow(theURL, winName, features) { //v2.0
+        open(theURL, winName, features);
+        return false;
+    }
+
+    // function openGoogleMap() {
+    //     open('https://www.google.com/maps/d/viewer?ll=5.332071,103.147845&source=embed&iwloc=00045067c9e420974965c&ie=UTF8&msa=0&spn=0,0&hl=en&mid=z-UdJ9CaRHZ8.kWNEoVqUrvDs');
+    //     return false;
+    // }
+
     $('#map1').click(function(evt) {
         MM_openBrWindow('images/map1.jpg','MAP1','scrollbars=yes,resizable=yes,width=750,height=610');
     });
